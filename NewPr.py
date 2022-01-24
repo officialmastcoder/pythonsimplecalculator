502ec16264e672e4a1b42aeaebac4976a7d0883a
AHK WEB SOLUTIONS <96109325+ahkwebsolutions@users.noreply.github.com>
2022-01-24 06:46:42 +0530

    Initial

---
+++ NewPr.py
@@ -0,0 +1,57 @@
+hello_name = "AHK AHK  WEB SOLUTIONS"
+
+
+## Print Any  String  In UpperCase
+##print(hello_name.upper())
+
+## Print Any String in Lowercase
+##print(hello_name.lower())
+
+## Replace Any  String  to ANy Other String
+##print(hello_name.replace("WEB", "WBE"))
+
+## Find Any String in  Any Variables
+##print(hello_name.find("s"))
+
+## ANother Methods of  replace The String
+
+##new_name = hello_name.replace("AHK", "AHK" ,2)
+## public
+ahkweb = hello_name
+
+## Check If  The  String  Exist Or Not
+##checkALetter = ("S" in ahkweb)
+##print(checkALetter)
+
+## Operators Function And Methods
+
+##i = 10
+##i = i + 5
+##print(i)
+
+## 2nd method
+##i = 10
+##i += 10
+##i -= 2
+##i *=5
+##i /=3
+##
+##print(i)
+
+
+
+
+## Simple calculator Using Python
+## Here I Am going to   calculate  Some Number
+num1 = input("Enter First Number : ")
+select_operator = input("Define What Yoiu Want to Do:  (+-/*)")
+num2 = input("Enter Second Number : ")
+
+## Calculate Usingf Operators
+if(select_operator  == "+" ): print(float(num1) + float(num2))
+if(select_operator == "-"): print(float(num1) - float(num2))
+if(select_operator == "*"): print(float(num1) * float(num2))
+if(select_operator == "/"): print(float(num1) / float(num2))
+if(select_operator == "%"): print(float(num1) / 100 * float(num2))
+
+##Simple Calculator In Python  Is Completed
